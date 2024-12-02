758544a463c765ddb61e1bd96ca81342190bc1c5
ysengoku <ysengoku.tourisme@gmail.com>
2024-12-02 22:42:36 +0100

    Update main loop for part 2

--- Day2/srcs/main.cpp
+++ Day2/srcs/main.cpp
@@ -12,18 +12,22 @@
 int main(int ac, char **av) {
   if (ac != 2) {
     std::cerr << "usage: " << av[0] << " file" << std::endl;
-	  return (1);
+    return (1);
   }
   std::vector< std::vector<int> > data = Parser(av[1]).getData();
   
   int count = 0;
-  std::vector< std::vector<int> >::iterator itEnd = data.end();
-  for (std::vector< std::vector<int> >::iterator it = data.begin(); it != itEnd; ++it) {
-    // printVector(*it);
-    if (SafeChecker::isSafe(*it)) {
+  for (size_t i = 0; i < data.size(); ) {
+    // printVector(data[i]);
+    if (SafeChecker::isSafe(data[i])) {
+      // std::cout << i << ": Safe" << std::endl;
+      data.erase(data.begin() + i);
       ++count;
+    } else {
+      ++i;
     }
   }
   std::cout << "Number of safe reports: " << count << std::endl;
+  std::cout << "Checking remaining " << data.size() << " reports..." << std::endl;
   return (0);
 }
